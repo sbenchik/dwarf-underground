f60399a144e6a22b36866ecdf91815509fd16f92
Steve Benchik <stevebenchik@gmail.com>
2017-05-23 09:16:35 -0400

    Extract navbar links into their own component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Links from './Links'
 import './Header.css'
+
+const links = ['Home', 'About', 'Log in']
 
 class Header extends Component{
     render(){
@@ -9,11 +12,7 @@ class Header extends Component{
             <div className="float-left logo">
               <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
             </div>
-            <div className="float-right nav-links">
-              <a href="#">Home</a>
-              <a href="#">About</a>
-              <a href="#">Log in</a>
-            </div>
+            <Links links={links}/>
           </div>
           <div className="headline text-center small-12 columns">
             <h1>The Dwarf Underground</h1>
---
+++ src/Header/Links.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.css'
+
+class NavLinks extends Component{
+    render(){
+        const links = this.props.links.map((link) => <a href="#">{link}</a>)
+        return(
+            <div className="float-right nav-links">
+              {links}
+            </div>
+        )
+    }
+}
+
+ export default NavLinks
